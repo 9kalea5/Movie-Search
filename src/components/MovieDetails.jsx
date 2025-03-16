d1019bf0e65f8890b090a6814afa46b9dcd100b7
kaleab <alebachewkaleab@gmail.com>
2025-03-16 15:48:26 +0300

    Movie Details

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import "./../styles/MovieDetails.css";
 
-export default function MovieDetails() {
+const MovieDetails = ({ selectedMovie, setSelectedMovie }) => {
   return (
-    <div>
-      
+    <div className="movie-details">
+      <h2>{selectedMovie.Title} ({selectedMovie.Year})</h2>
+      <img src={selectedMovie.Poster} alt={selectedMovie.Title} />
+      <p><strong>Genre:</strong> {selectedMovie.Genre}</p>
+      <p><strong>Plot:</strong> {selectedMovie.Plot}</p>
+      <p><strong>Director:</strong> {selectedMovie.Director}</p>
+      <p><strong>Actors:</strong> {selectedMovie.Actors}</p>
+      <p><strong>IMDb Rating:</strong> {selectedMovie.imdbRating}</p>
+      <button onClick={() => setSelectedMovie(null)}>Back to Search</button>
     </div>
-  )
-}
+  );
+};
+
+export default MovieDetails;
