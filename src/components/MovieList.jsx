4cdeecf6803baca3ee7ed4fbf62eab2e93ca377f
kaleab <alebachewkaleab@gmail.com>
2025-03-16 15:47:36 +0300

    Movie List

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import MovieCard from "./MovieCard";
+import "./../styles/MovieList.css";
 
-export default function MovieList() {
+const MovieList = ({ movies, fetchMovieDetails, page, totalPages, searchMovies }) => {
   return (
     <div>
-      
+      <div className="movie-list">
+        {movies.map((movie) => (
+          <MovieCard key={movie.imdbID} movie={movie} fetchMovieDetails={fetchMovieDetails} />
+        ))}
+      </div>
+
+      {/* Pagination */}
+      <div className="pagination">
+        {page > 1 && <button onClick={() => searchMovies(page - 1)}>Previous</button>}
+        <span> Page {page} of {totalPages} </span>
+        {page < totalPages && <button onClick={() => searchMovies(page + 1)}>Next</button>}
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default MovieList;
